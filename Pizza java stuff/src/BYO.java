741458d5a2115ba89340830a485481ce198d3fc5
Ray Rosario <rxrosario1117@gmail.com>
2022-04-07 17:55:03 -0400

    Added BYO class. Changed Pizza class to better handle creation of new pizzas.

---
+++ Pizza java stuff/src/BYO.java
@@ -0,0 +1,25 @@
+import java.util.List;
+
+public class BYO extends Pizza{
+    private String crustType;
+    private String sauce;
+    private String cheese;
+    private List<String> toppings;
+
+    public BYO(String crustType, String sauce, String cheese, List<String> toppings, String size) {
+        super(size);
+        this.crustType = crustType;
+        this.sauce = sauce;
+        this.cheese = cheese;
+        this.toppings = toppings;
+        setPrice(getPrice() + calcToppingPrice());
+
+    }
+
+    public float calcToppingPrice(){
+        return (.50f * toppings.size());
+    }
+}
+
+
+
